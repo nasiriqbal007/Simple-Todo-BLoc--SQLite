bf931dd8b2f2070f41ef5d91b4224bd0b51e2640
Nasir Iqbal <nasiriqbal1198@gmail.com>
2024-12-24 12:27:07 +0500

    comments changes

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: TodoPage(todoRepository: todoRepository), // Pass the repository directly
+      home: TodoPage(todoRepository: todoRepository),
     );
   }
 }
